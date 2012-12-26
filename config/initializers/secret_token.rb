5c82617c92e40254c5018f07210de7d733d3898d
xieyu33333 <xieyu33333@gmail.com>
2012-12-26 21:32:58 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'c00c86f832b5af497547a3bdd63e350aec7f0cd2519135b16c53abd22206b5e4b6b354fbd03de75df2c4ce741e4ab1e2d9e2045fbc46deb3cf0b5f84bd174506'
---
+++ db/seeds.rb
@@ -0,0 +1,16 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.delete_all
+Product.create(
+:title => 'My world of tank experience',
+:description =>%{<p>These are the new guides for Rails 3.2 based on v3.2.9. 
+	These guides are designed to make you immediately productive with Rails,
+	 and to help you understand how all of the pieces fit together.</p>},
+:img_url => 'ruby.jpg',
+:price => 49.50	 )
+
